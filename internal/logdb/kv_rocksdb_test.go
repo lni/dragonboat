0300955b1b726e3a94cace3c0380a8ec3b0be13a
Lei Ni <nilei81@gmail.com>
2019-01-21 23:55:38 +0800

    Removed lmdb support.

--- internal/logdb/kv_rocksdb_test.go
+++ internal/logdb/kv_rocksdb_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build !dragonboat_lmdb,!dragonboat_leveldb
+// +build !dragonboat_leveldb
 
 package logdb
 
