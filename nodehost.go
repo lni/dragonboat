8efd11134471291e0467d6f7cb9c4edd8eee5c6b
Lei Ni <nilei81@gmail.com>
2019-04-05 16:27:11 +0800

    dragonboat: bumped version number to v2.1.5.

--- nodehost.go
+++ nodehost.go
@@ -104,7 +104,7 @@ const (
 	// DragonboatMinor is the minor version number
 	DragonboatMinor = 1
 	// DragonboatPatch is the patch version number
-	DragonboatPatch = 2
+	DragonboatPatch = 5
 	// DEVVersion is a boolean flag indicating whether this is a dev version
 	DEVVersion = false
 )
