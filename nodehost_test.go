3d8fa7f51c862a5f7314bd12d14053e976782f98
Lei Ni <nilei81@gmail.com>
2019-06-14 13:02:06 +0800

    dragonboat: simplified some tests

--- nodehost_test.go
+++ nodehost_test.go
@@ -37,6 +37,7 @@ import (
 	"time"
 
 	"github.com/lni/dragonboat/config"
+	"github.com/lni/dragonboat/internal/logdb"
 	"github.com/lni/dragonboat/internal/rsm"
 	"github.com/lni/dragonboat/internal/server"
 	"github.com/lni/dragonboat/internal/settings"
@@ -2483,6 +2484,162 @@ func TestNodeHostReturnsErrorWhenLogDBCanNotBeCreated(t *testing.T) {
 	if err != server.ErrIncompatibleData {
 		t.Fatalf("failed to return ErrIncompatibleData")
 	}
+}
+
+func TestBatchedAndPlainEntriesAreNotCompatible(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	defer os.RemoveAll(singleNodeHostTestDir)
+	nhc := config.NodeHostConfig{
+		WALDir:         singleNodeHostTestDir,
+		NodeHostDir:    singleNodeHostTestDir,
+		RTTMillisecond: 100,
+		RaftAddress:    nodeHostTestAddr1,
+		LogDBFactory:   logdb.NewDefaultBatchedLogDB,
+	}
+	plog.Infof("going to create nh using batched logdb")
+	nh, err := NewNodeHost(nhc)
+	if err != nil {
+		t.Fatalf("failed to create node host %v", err)
+	}
+	bf := nh.logdb.BinaryFormat()
+	if bf != raftio.LogDBBinVersion {
+		t.Errorf("unexpected logdb bin ver %d", bf)
+	}
+	nh.Stop()
+	plog.Infof("node host 1 stopped")
+	nhc.LogDBFactory = logdb.NewDefaultLogDB
+	func() {
+		plog.Infof("going to create nh using plain logdb")
+		nh, err := NewNodeHost(nhc)
+		plog.Infof("err : %v", err)
+		if err != server.ErrLogDBBrokenChange {
+			if err == nil && nh != nil {
+				plog.Infof("going to stop nh")
+				nh.Stop()
+			}
+			t.Fatalf("didn't return the expected error")
+		}
+	}()
+	os.RemoveAll(singleNodeHostTestDir)
+	plog.Infof("going to create nh using plain logdb with existing data deleted")
+	nh, err = NewNodeHost(nhc)
+	plog.Infof("err2 : %v", err)
+	if err != nil {
+		t.Fatalf("failed to create node host %v", err)
+	}
+	defer nh.Stop()
+	bf = nh.logdb.BinaryFormat()
+	if bf != raftio.PlainLogDBBinVersion {
+		t.Errorf("unexpected logdb bin ver %d", bf)
+	}
+}
+
+func TestNodeHostReturnsErrLogDBBrokenChangeWhenLogDBTypeChanges(t *testing.T) {
+	defer os.RemoveAll(singleNodeHostTestDir)
+	nhc := config.NodeHostConfig{
+		NodeHostDir:    singleNodeHostTestDir,
+		RTTMillisecond: 200,
+		RaftAddress:    nodeHostTestAddr1,
+		LogDBFactory:   logdb.NewDefaultBatchedLogDB,
+	}
+	func() {
+		nh, err := NewNodeHost(nhc)
+		if err != nil {
+			t.Fatalf("failed to create nodehost %v", err)
+		}
+		defer nh.Stop()
+	}()
+	nhc.LogDBFactory = logdb.NewDefaultLogDB
+	_, err := NewNodeHost(nhc)
+	if err != server.ErrLogDBBrokenChange {
+		t.Fatalf("failed to return ErrIncompatibleData")
+	}
+}
+
+func TestNodeHostByDefaultUsePlainEntryLogDB(t *testing.T) {
+	defer os.RemoveAll(singleNodeHostTestDir)
+	nhc := config.NodeHostConfig{
+		NodeHostDir:    singleNodeHostTestDir,
+		RTTMillisecond: 20,
+		RaftAddress:    nodeHostTestAddr1,
+		LogDBFactory:   logdb.NewDefaultLogDB,
+	}
+	func() {
+		nh, err := NewNodeHost(nhc)
+		if err != nil {
+			t.Fatalf("failed to create nodehost %v", err)
+		}
+		defer nh.Stop()
+		rc := config.Config{
+			NodeID:       1,
+			ClusterID:    1,
+			ElectionRTT:  3,
+			HeartbeatRTT: 1,
+		}
+		peers := make(map[uint64]string)
+		peers[1] = nodeHostTestAddr1
+		newPST := func(clusterID uint64, nodeID uint64) sm.IStateMachine {
+			return &PST{}
+		}
+		if err := nh.StartCluster(peers, false, newPST, rc); err != nil {
+			t.Fatalf("failed to start cluster %v", err)
+		}
+		waitForLeaderToBeElected(t, nh, 1)
+		cs := nh.GetNoOPSession(1)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		_, err = nh.SyncPropose(ctx, cs, []byte("test-data"))
+		cancel()
+		if err != nil {
+			t.Fatalf("failed to make proposal %v", err)
+		}
+	}()
+	nhc.LogDBFactory = logdb.NewDefaultBatchedLogDB
+	_, err := NewNodeHost(nhc)
+	if err != server.ErrIncompatibleData {
+		t.Fatalf("failed to return server.ErrIncompatibleData")
+	}
+}
+
+func TestNodeHostByDefaultChecksWhetherToUseBatchedLogDB(t *testing.T) {
+	defer os.RemoveAll(singleNodeHostTestDir)
+	nhc := config.NodeHostConfig{
+		NodeHostDir:    singleNodeHostTestDir,
+		RTTMillisecond: 20,
+		RaftAddress:    nodeHostTestAddr1,
+		LogDBFactory:   logdb.NewDefaultBatchedLogDB,
+	}
+	tf := func() {
+		nh, err := NewNodeHost(nhc)
+		if err != nil {
+			t.Fatalf("failed to create nodehost %v", err)
+		}
+		defer nh.Stop()
+		rc := config.Config{
+			NodeID:       1,
+			ClusterID:    1,
+			ElectionRTT:  3,
+			HeartbeatRTT: 1,
+		}
+		peers := make(map[uint64]string)
+		peers[1] = nodeHostTestAddr1
+		newPST := func(clusterID uint64, nodeID uint64) sm.IStateMachine {
+			return &PST{}
+		}
+		if err := nh.StartCluster(peers, false, newPST, rc); err != nil {
+			t.Fatalf("failed to start cluster %v", err)
+		}
+		waitForLeaderToBeElected(t, nh, 1)
+		cs := nh.GetNoOPSession(1)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		_, err = nh.SyncPropose(ctx, cs, []byte("test-data"))
+		cancel()
+		if err != nil {
+			t.Fatalf("failed to make proposal %v", err)
+		}
+	}
+	tf()
+	nhc.LogDBFactory = logdb.NewDefaultLogDB
+	tf()
 }
 
 func TestNodeHostWithUnexpectedDeploymentIDWillBeDetected(t *testing.T) {
