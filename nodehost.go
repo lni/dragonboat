c25f02d8b2f237e50b37c523725ca060695840b7
Lei Ni <nilei81@gmail.com>
2019-06-21 14:28:44 +0800

    dragonboat: removed the dev version tag

--- nodehost.go
+++ nodehost.go
@@ -115,7 +115,7 @@ const (
 	// DragonboatPatch is the patch version number
 	DragonboatPatch = 0
 	// DEVVersion is a boolean flag indicating whether this is a dev version
-	DEVVersion = true
+	DEVVersion = false
 )
 
 var (
