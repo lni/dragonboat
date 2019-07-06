9ceac7debb0114ace0ddac2b344068e9a4139a52
Lei Ni <nilei81@gmail.com>
2019-07-06 11:49:47 +0000

    etc: use VictoriaMetrics 1.6.2

--- config/config_test.go
+++ config/config_test.go
@@ -19,7 +19,6 @@ import (
 )
 
 func ExampleNodeHostConfig() {
-	// import "github.com/lni/dragonboat/v3/plugin/rpc"
 	nhc := NodeHostConfig{
 		WALDir:         "/data/wal",
 		NodeHostDir:    "/data/dragonboat-data",
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/lni/dragonboat/v3
 
 require (
-	github.com/VictoriaMetrics/metrics v1.5.0
+	github.com/VictoriaMetrics/metrics v1.6.2
 	github.com/golang/protobuf v1.2.0
 )
