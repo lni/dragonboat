123257efa3e0c2a4a809e78a3c375f7300e365ce
Lei Ni <nilei81@gmail.com>
2020-08-07 16:34:23 +0800

    rsm: added a test TestSessionSaveOrderWithEviction

--- internal/rsm/sessionmanager_test.go
+++ internal/rsm/sessionmanager_test.go
@@ -15,7 +15,12 @@
 package rsm
 
 import (
+	"bytes"
 	"testing"
+
+	"github.com/lni/goutils/cache"
+
+	sm "github.com/lni/dragonboat/v3/statemachine"
 )
 
 func TestRegisteriAndUnregisterClient(t *testing.T) {
@@ -46,4 +51,61 @@ func TestRegisteriAndUnregisterClient(t *testing.T) {
 	if h1 != h3 {
 		t.Errorf("hash unexpectedly changed")
 	}
+}
+
+func TestSessionSaveOrderWithEviction(t *testing.T) {
+	sm1 := &SessionManager{sessions: newLRUSession(4)}
+	sm2 := &SessionManager{sessions: newLRUSession(4)}
+
+	for i := uint64(0); i < sm1.sessions.size; i++ {
+		sm1.RegisterClientID(i)
+	}
+	// touch the oldest session to make it the most recently accessed
+	s, ok := sm1.ClientRegistered(uint64(0))
+	if !ok {
+		t.Fatalf("failed to get client session")
+	}
+	sm1.AddResponse(s, 1, sm.Result{Value: 123456})
+	ss := &bytes.Buffer{}
+	if err := sm1.SaveSessions(ss); err != nil {
+		t.Fatalf("failed to save snapshot %v", err)
+	}
+	rs := bytes.NewBuffer(ss.Bytes())
+	if err := sm2.LoadSessions(rs, V2SnapshotVersion); err != nil {
+		t.Fatalf("failed to restore snapshot %v", err)
+	}
+	// client with the same client id (1 and 2 here) expected to be evicted
+	sm1.RegisterClientID(sm1.sessions.size)
+	sm2.RegisterClientID(sm1.sessions.size)
+	sm1.RegisterClientID(sm1.sessions.size + 1)
+	sm2.RegisterClientID(sm1.sessions.size + 1)
+	s1 := &bytes.Buffer{}
+	if err := sm1.SaveSessions(s1); err != nil {
+		t.Fatalf("failed to save snapshot %v", err)
+	}
+	s2 := &bytes.Buffer{}
+	if err := sm2.SaveSessions(s2); err != nil {
+		t.Fatalf("failed to save snapshot %v", err)
+	}
+	if !bytes.Equal(s1.Bytes(), s2.Bytes()) {
+		t.Fatalf("different snapshot")
+	}
+	check := func(c *cache.OrderedCache) {
+		keys := make(map[uint64]struct{})
+		c.OrderedDo(func(k, v interface{}) {
+			clientID := k.(*RaftClientID)
+			keys[uint64(*clientID)] = struct{}{}
+		})
+		if _, ok := keys[0]; !ok {
+			t.Errorf("client 0 not in the session list")
+		}
+		if _, ok := keys[1]; ok {
+			t.Errorf("client 1 unexpectedly still in the session list")
+		}
+		if _, ok := keys[2]; ok {
+			t.Errorf("client 2 unexpectedly still in the session list")
+		}
+	}
+	check(sm1.sessions.sessions)
+	check(sm2.sessions.sessions)
 }
