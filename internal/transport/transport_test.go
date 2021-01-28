a476cc407975ee55c7611081dc83d2a69fcf5ff2
Lei Ni <nilei81@gmail.com>
2021-01-28 14:10:27 +0800

    tests: minor update for a test

--- internal/transport/transport_test.go
+++ internal/transport/transport_test.go
@@ -1305,7 +1305,7 @@ func TestCircuitBreakerForResolveNotShared(t *testing.T) {
 	if ok := tt.Send(msg); !ok {
 		t.Errorf("send failed")
 	}
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 20; i++ {
 		if ok := tt.Send(msgUnknownNode); ok {
 			t.Errorf("send unexpectedly returned ok")
 		}
